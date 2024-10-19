394e3c12f77122c0ffb0186f3e3b4a454b91dd9a
Chupssss <eugene126@yandex.ru>
2024-10-19 16:39:39 +0300

    main file bot

--- bot_main.py
+++ bot_main.py
@@ -1,1 +1,25 @@
-print("lets_go")
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+import paramiko
+import json
+
+with open("config.json", "r") as config_file:
+    config = json.load(config_file)
+
+with open("servers.json", "r") as servers_list:
+    servers = json.load(servers_list)
+
+with open("user_data.json", "r") as users_data_file:
+    users_data = json.load(users_data_file)
+
+
+bot = telebot.TeleBot(config["API_token"])
+
+
+def is_admin(message):
+    if message.from_user.id in config["admins"]:
+        return True
+    else:
+        return False
+
+def save_user_data
